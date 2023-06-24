1786e3745165879159173bf441cd43e0ee916e93
agnesfiaa <94664966+agnesfiaa@users.noreply.github.com>
2023-06-24 18:24:24 +0700

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://incredible-meringue-984ed9.netlify.app/" // TODO: replace this
+const studentName = "AGNESFIA ANGGRAENI" // TODO: replace this
+const studentId = "FE4930900" // TODO: replace this
+
+export { url, studentName, studentId }
